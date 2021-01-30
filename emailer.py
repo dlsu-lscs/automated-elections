ed611478301e97524283c03a7bec535f19ca4f9d
silentrald <silencommentator1216@gmail.com>
2021-01-30 12:57:00 +0800

    Changed Emailer

--- emailer.py
+++ emailer.py
@@ -9,11 +9,15 @@ ex.
     python emailer.py CCS
 """
 
+import smtplib
 import environ
 import os
 from random import randint
 from tqdm import tqdm
 from sys import argv
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 import django
 from django.conf import settings
@@ -25,6 +29,7 @@ if len(argv) < 2:
 env = environ.Env()
 environ.Env.read_env('autoelect/settings/.env')
 
+#### DJANGO SETUP
 settings.configure(
     BASE_DIR = os.path.dirname(os.path.abspath(__file__)),
 
@@ -54,10 +59,18 @@ settings.configure(
 )
 django.setup()
 
-from email.mime.image import MIMEImage
-from django.core.mail import EmailMultiAlternatives
 from vote.models import AuthUser as User, Voter, College
 
+def generate_passcode():
+    length = 8
+    charset = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789'
+    passcode = ''
+
+    for index in range(length):
+        passcode += charset[randint(0, len(charset) - 1)]
+    return passcode
+
+##### INPUT VALIDATION
 # Get all voters
 colleges = argv[1:]
 
@@ -76,34 +89,36 @@ if voters.count() == 0:
     print('No voters found')
     exit()
 
-def generate_passcode():
-    length = 8
-    charset = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789'
-    passcode = ''
-
-    for index in range(length):
-        passcode += charset[randint(0, len(charset) - 1)]
-    return passcode
-
-# EMAIL BODY CONST
+######### CONTENT CREATION
+# HTML
 fp = open(settings.BASE_DIR + '/email_template.html', 'r')
 HTML_STR = fp.read()
 fp.close()
 
-# Image
+# IMAGE
 fp = open(settings.BASE_DIR + '/ComelecLogo.png', 'rb')
 img = MIMEImage(fp.read())
 fp.close()
+img.add_header('Content-ID', '<logo>')
 
-size = len(voters)
+######### EMAILER SETUP
+s = smtplib.SMTP('mail.usg-election.com', 587)
+s.ehlo()
+s.starttls()
+s.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
 
-fp = open(settings.BASE_DIR + '/deletethis.csv', 'w')
+# fp = open(settings.BASE_DIR + '/deletethis.csv', 'w')
 n = 500
 
-for i in tqdm(range(size)):
-    if i % n == 0:
-        fp.close()
-        fp = open(settings.BASE_DIR + '/accounts_' + str(i // n) + '.csv', 'w')
+import time
+
+# for i in tqdm(range(skip, size)):
+# for i in tqdm(range(size)):
+for i in range(1):
+    # time.sleep(10)
+    # if i % n == 0:
+    #     fp.close()
+    #     fp = open(settings.BASE_DIR + '/accounts_' + str(i // n) + '.csv', 'w')
 
     voter = voters[i]
 
@@ -113,12 +128,9 @@ for i in tqdm(range(size)):
     voter.set_password(new_password)
     voter.save()
 
-    fp.write(voter.username + ',' + new_password + '\n')
-
-    img.add_header('Content-ID', '<logo>')
+    # fp.write(voter.username + ',' + new_password + '\n')
 
     # TODO: Change URL
-    subject = '[COMELEC] Election is now starting'
     text = '''\
 DLSU Comelec is inviting to you to vote in the elections.
 Voter ID: {}
@@ -131,18 +143,20 @@ To vote, go to this link: https://usg-election.dlsu.edu.ph/login
     html = html.replace('11xxxxxx', voter.username, 2)
     html = html.replace('xxxxxxxx', new_password, 1)
 
-    msg = EmailMultiAlternatives(
-        subject = subject,
-        body = text,
-        from_email = 'usg.election@gmail.com',
-        to = [ voter.email ]
-    )
-    msg.attach_alternative(html, "text/html")
-    msg.attach(img)
-    
     try:
-        msg.send()
-    except:
-        print('Email did not sent for ' + voter.username)
+        msg             = MIMEMultipart('alternative')
+        msg['From']     = 'DLSU COMELEC<{}>'.format(settings.EMAIL_HOST_USER)
+        # TODO: ID NUMBER HERE
+        msg['To']       = '@dlsu.edu.ph'
+        msg['Subject']  = '[COMELEC] Election is now starting'
 
+        msg.attach(MIMEText(text, 'text'))
+        msg.attach(MIMEText(html, 'html'))
+        msg.attach(img)
+        s.send_message(msg)
+    except Exception as e:
+        print(e)
+        print('Email did not sent for {}'.format(voter.username))
+
+s.close()
 fp.close()
--- test.py
+++ test.py
@@ -1,9 +1,50 @@
-fp = open('deletethis.csv', 'w')
-for i in range(18):
-    if i % 10 == 0:
-        fp.close()
-        fp = open('asdf' + str(i // 10) + '.csv', 'w')
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-    fp.write('hello' + str(i) + '\n')
+s = smtplib.SMTP('mail.usg-election.com', 587)
+s.ehlo()
+s.starttls()
+s.login('comelec@usg-election.com', 'BZL-LAcUaPa1Vs5b')
 
+subject = '[COMELEC] Election is now starting'
+text = '''\
+DLSU Comelec is inviting to you to vote in the elections.
+Voter ID: 11827211
+Voter Key: hi
+To vote, go to this link: https://usg-election.dlsu.edu.ph/login
+    '''
+
+#Setting Email Parameters
+msg             = MIMEMultipart('alternative')
+msg['From']     = 'DLSU COMELEC<comelec@usg-election.com>'
+msg['To']       = '11827211@dlsu.edu.ph'
+msg['Subject']  = subject
+
+#Email Body Content
+fp = open('email_template.html', 'r')
+HTML_STR = fp.read()
 fp.close()
+
+html = HTML_STR
+html = html.replace('11xxxxxx', '11827211', 2)
+html = html.replace('xxxxxxxx', 'HEELLOO00', 1)
+
+#Add Message To Email Body
+fp = open('ComelecLogo.png', 'rb')
+img = MIMEImage(fp.read())
+fp.close()
+img.add_header('Content-ID', '<logo>')
+
+msg.attach(MIMEText(text, 'text'))
+msg.attach(MIMEText(html, 'html'))
+msg.attach(img)
+
+try:
+    s.send_message(msg)
+except Exception as e:
+    print(e)
+    print('Did not send')
+
+s.quit()
